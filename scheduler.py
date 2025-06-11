88ffad4a22077f5675e1abfc0e6b4c4265bbe8ed
Dmitry <repitox@gmail.com>
2025-06-11 15:58:09 +0300

    обновление шедулера

--- scheduler.py
+++ scheduler.py
@@ -45,5 +45,5 @@ def send_message(user_id, text):
 # Запуск планировщика
 def start_scheduler():
     scheduler = BackgroundScheduler(timezone=pytz.timezone("Europe/Moscow"))
-    scheduler.add_job(send_daily_summary, "cron", hour=18, minute=15)
+    scheduler.add_job(send_daily_summary, "cron", hour=16, minute=1)
     scheduler.start()
